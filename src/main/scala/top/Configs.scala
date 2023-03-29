88b405c24f65ddc70ab271d4789c552260bcfec2
hukangjia <hukangjia919@163.com>
2023-03-29 19:44:13 +0800

    a error

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -12,7 +12,6 @@
 * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 *
 * See the Mulan PSL v2 for more details.
-  * //修改了
 ***************************************************************************************/
 
 package top
